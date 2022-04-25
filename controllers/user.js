931cbc0561bcdcb54df636ab360894cf2d168cd7
Paula <paula.sua.008@gmail.com>
2022-04-24 21:25:54 -0400

    Token Add Custom

--- controllers/user.js
+++ controllers/user.js
@@ -2,22 +2,22 @@ const { default: axios } = require('axios');
 const { response } = require('express'); 
 
 const getUsers = async(req, res = response) => {
-    const {limit,offset} = req.query;
-    const resp = await axios.get(`https://comicvine.gamespot.com/api/issues/?api_key=5a6f9e2c8be618cd1007b31fc422403b8c02a7bd&format=json&limit=${limit}&offset=${offset}`)
+    const {limit,offset,token} = req.query;
+    const resp = await axios.get(`https://comicvine.gamespot.com/api/issues/?api_key=${token}&format=json&limit=${limit}&offset=${offset}`)
     res.json(resp.data);
 }
 
 
 //http://localhost:4000/api/comic/detail?cod=4000-8
 const getComic = async(req, res = response) => {
-    const {cod} = req.query;
-    const resp = await axios.get(`https://comicvine.gamespot.com/api/issue/${cod}/?api_key=5a6f9e2c8be618cd1007b31fc422403b8c02a7bd&format=json`)
+    const {cod,token} = req.query;
+    const resp = await axios.get(`https://comicvine.gamespot.com/api/issue/${cod}/?api_key=${token}&format=json`)
     res.json(resp.data);
 }
 
 const getThumb = async(req, res = response) => {
-    const {cod,category} = req.query;
-    const resp = await axios.get(`https://comicvine.gamespot.com/api/${category}/${cod}/?api_key=5a6f9e2c8be618cd1007b31fc422403b8c02a7bd&format=json`)
+    const {cod,category,token} = req.query;
+    const resp = await axios.get(`https://comicvine.gamespot.com/api/${category}/${cod}/?api_key=${token}&format=json`)
     res.json(resp.data);
 }
 
